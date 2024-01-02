6f987da6ee3723aa7e2eb2fe18ae762bb5c20b25
Carlos Buruel Ortiz <carlos.ob.26@gmail.com>
2024-01-02 08:14:06 -0600

    Inicializar la aplicacion de Firebase en Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,11 @@ import 'package:push_app/config/router/app_router.dart';
 import 'package:push_app/config/theme/app_theme.dart';
 import 'package:push_app/presentation/bloc/notification/notification_bloc.dart';
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await NotificationBloc.initializeFCM();
+
   runApp(
     MultiBlocProvider(
       providers: [
--- lib/presentation/bloc/notification/notification_bloc.dart
+++ lib/presentation/bloc/notification/notification_bloc.dart
@@ -1,6 +1,9 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:push_app/firebase_options.dart';
 
 part 'notification_event.dart';
 part 'notification_state.dart';
@@ -12,6 +15,12 @@ class NotificationBloc extends Bloc<NotificationEvent, NotificationState> {
     // on<NotificationEvent>((event, emit) {
     //   // TODO: implement event handler
     // });
+  }
+
+  static Future<void> initializeFCM() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
   }
 
   void requestPermission() async {
