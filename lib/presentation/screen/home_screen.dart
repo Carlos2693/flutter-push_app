e35aeeea456680ddb18f669707499e847e0f87a8
Carlos Buruel Ortiz <carlos.ob.26@gmail.com>
2024-01-03 08:22:30 -0600

    331. Navegar a la segunda pantalla

--- lib/presentation/screen/home_screen.dart
+++ lib/presentation/screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 import 'package:push_app/presentation/bloc/notification/notification_bloc.dart';
 
@@ -45,6 +46,9 @@ class _HomeView extends StatelessWidget {
           leading: notification.imageUrl != null
             ? Image.network(notification.imageUrl!)
             : const Text('No image'),
+          onTap: () {
+            context.push('/push-details/${notification.messageId}');
+          },
         );
       }
     );
